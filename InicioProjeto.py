585d9c3113ba2e283bad407ef5375fb3e156424a
LucaAlbuquerque <lra3@cesar.school>
2024-10-24 13:49:41 -0300

    fodase

--- InicioProjeto.py
+++ InicioProjeto.py
@@ -46,3 +46,59 @@ while opc!=5:
             print(f"Localização: {localizacao}")
             print(f"Condição climática: {condicoes_climaticas}")
             i+=1
+        if adc=="c":
+            treino_comp.append(f"C{j+1}")
+            data = input(" Digite a data da ocasião (formato DD-MM-YYYY): ") 
+            distancia = float(input(" Digite a distância percorrida (em KM): "))
+            tempo = float(input(" Digite o tempo de duração (em Horas): "))
+            localizacao = input(" Digite a localização do lugar: ")
+            condicoes_climaticas =input("\tNEVE \tCHUVA \tENSOLARADO \tNUBLADO \nDigite a condição climática: ")
+
+            if len(data) == 10 and data[2] == '-' and data[5] == '-':
+                ano = int(data[6:10])
+                mes = int(data[3:5])
+                dia = int(data[0:2])
+
+                if 0 < mes < 13 and 0 < dia < 32:
+                    print(f" A data da ocasião é {dia}/{mes}/{ano}")
+                else:
+                    print(" Data inválida ")
+            else:
+                print(" Formato inválido ")
+
+            print(f" Distância: {distancia} Km ")
+            print(f" O tempo de duração da ocasião será de {tempo} horas ")
+            print(f" A localização da ocasião será na {localizacao} ")
+            print(f" Condição climática: {condicoes_climaticas}")
+            j+=1
+    elif opc==2:
+        for i in treino_comp:
+            print(i,end=" ")
+        print("\nData:")
+        for i in v_data:
+            print(i)
+        print("Distância: ")
+        for i in v_dist:
+            print(i)
+        print("Tempo: ")
+        for i in v_tempo:
+            print(i)
+        print("Localização: ")
+        for i in v_loc:
+            print(i)
+        print("Condição climática: ")
+        for i in v_condc:
+            print(i)
+
+    elif opc==3:
+        updt=int(input(("Digite qual registro você deseja atualizar: ")))
+        nv=(input("Digite o novo registro: "))
+        treino_comp[updt-1]=nv
+    elif opc==4:
+        exc=(input("Digite qual o registro que você deseja excluir: "))
+        treino_comp.remove(exc)
+    elif opc==5:
+        break
+    else:
+        print("Código inválido")
+
